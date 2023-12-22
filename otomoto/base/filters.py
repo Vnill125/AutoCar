1ae9eefcf3708e35c57c6f711696818bd1335347
Krystian Muszynski <129762972+Vnill125@users.noreply.github.com>
2023-12-22 13:43:16 +0100

    Update filters.py

--- otomoto/base/filters.py
+++ otomoto/base/filters.py
@@ -1,9 +1,12 @@
 import django_filters
-from .models import Car
+from django.forms.widgets import TextInput
+from .models import Car, Car_Brand, Car_Model
 
 class CarFilter(django_filters.FilterSet):
-    max_price = django_filters.NumberFilter(field_name='max_price', lookup_expr='lte')
-
+    max_price = django_filters.NumberFilter(field_name='max_price', lookup_expr='lte', label='Price', widget=TextInput(attrs={'placeholder': 'Price to:'}))
+    first_registration_year = django_filters.CharFilter(widget=TextInput(attrs={'placeholder': 'Year:'}))
+    
     class Meta:
         model = Car
         fields = ['brand', 'model', 'body', 'first_registration_year', 'horse_power', 'transmission', 'fuel_type', 'price', 'max_price']
+        
