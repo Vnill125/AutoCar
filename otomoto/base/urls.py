c9b5766d07634729a8f1c08a423db171fb8df73e
Krystian Muszynski <129762972+Vnill125@users.noreply.github.com>
2023-12-22 13:42:53 +0100

    Update urls.py

--- otomoto/base/urls.py
+++ otomoto/base/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from . import views
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path("register/", views.register ,name="register")
 ]
