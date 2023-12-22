a192530e995508d9ade49c22b5a7b3f651dbfc6f
Krystian Muszynski <129762972+Vnill125@users.noreply.github.com>
2023-12-22 13:43:05 +0100

    Update models.py

--- otomoto/base/models.py
+++ otomoto/base/models.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.core.exceptions import ValidationError
 
 import pytz
 import datetime
+
 
 def validate_registration_year(year):
     if year < 1900 or year > 2024:
@@ -49,10 +51,9 @@ class Car(models.Model):
 
     brand = models.ForeignKey(Car_Brand, on_delete=models.CASCADE)
     model = models.ForeignKey(Car_Model, on_delete=models.CASCADE)
-
+    
     body = models.CharField(max_length=255, null=False, blank=False, choices=BODY_TYPE)
-    first_registration_year = models.IntegerField(
-        null=False, blank=False, default=datetime.datetime.today().year, validators=[validate_registration_year])
+    first_registration_year = models.IntegerField(null=False, blank=False, default=datetime.datetime.today().year, validators=[validate_registration_year])
     horse_power = models.IntegerField(null=False, blank=False, default=None)
     transmission = models.CharField(max_length=255, choices=TRANSMISSION_TYPE, default=None)
     fuel_type = models.CharField(max_length=255, null=False, blank=False, default='None', choices=FUEL_TYPE)
