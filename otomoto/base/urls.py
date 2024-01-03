30aab68550e390de956e0472fe4f054ccacb714a
Krystian Muszynski <129762972+Vnill125@users.noreply.github.com>
2024-01-03 23:53:53 +0100

    Update urls.py

--- otomoto/base/urls.py
+++ otomoto/base/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout_user, name='logout'),
-    path("register/", views.register ,name="register")
+    path("register/", views.register ,name="register"),
+    path('car/<str:pk>/', views.car, name='car'),
+    path('new-order/', views.new_order, name='new_order'),
+    path('chat/', views.new_order, name='new_order')
 ]
